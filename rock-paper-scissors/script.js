99e3da027b1ecbfef4fcd4da9b63a4d5951c7751
LukaSirotkovic <lukasirotkovic5@gmail.com>
2025-06-21 23:44:50 +0200

    Created UI for RPS game.

--- rock-paper-scissors/script.js
+++ rock-paper-scissors/script.js
@@ -1,74 +1,108 @@
- function getComputerChoice() {
-        const minCeiled = Math.ceil(0);
-        const maxFloored = Math.floor(3);
-        const randomNumber = Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled);
+let humanScore = 0;
+let computerScore = 0;
 
-        if (randomNumber === 0) {
-            return "rock"
-        }
-        else if (randomNumber === 1) {
-            return "paper"
-        }
-        else {
-            return "scissors"
-        }
+let counter = 0;
+
+let humanScoreUI = document.querySelector(".human-points span");
+let computerScoreUI = document.querySelector(".computer-points span");
+let pedestal = document.querySelector(".pedestal p");
+
+let userChoiceUI = document.querySelector(".human-choice span");
+let computerChoiceUI = document.querySelector(".computer-choice span");
+
+let playAgainButton = document.querySelector(".pedestal .play-again");
+
+function getComputerChoice() {
+    const choices = ["rock", "paper", "scissors"];
+    return choices[Math.floor(Math.random() * choices.length)];
+}
+
+function playRound(userChoice, computerChoice) {
+    userChoice = userChoice.toLowerCase();
+    computerChoice = computerChoice.toLowerCase();
+
+    if (
+        (userChoice === "rock" && computerChoice === "scissors") ||
+        (userChoice === "scissors" && computerChoice === "paper") ||
+        (userChoice === "paper" && computerChoice === "rock")
+    ) {
+        humanScore++;
+        pedestal.textContent = "Nice you got a point!"
+        humanScoreUI.textContent = humanScore;
     }
 
-    function getHumanChoice() {
-        return prompt("Type Rock Paper or Scissors: ")
+    else if (userChoice != computerChoice) {
+        computerScore++
+        pedestal.textContent = "Uff, computer got a point!"
+        computerScoreUI.textContent = computerScore;
     }
 
-    function playRound(userChoice, computerChoice) {
-        userChoice = userChoice.toLowerCase();
-        computerChoice = computerChoice.toLowerCase();
+    console.log("Human points: ", humanScore, "Computer points: ", computerScore)
+}
 
-        if (userChoice === "rock" && computerChoice === "scissors") {
-            humanScore++;
-        }
-        else if (userChoice === "scissors" && computerChoice === "paper") {
-            humanScore++;
-        }
-        else if (userChoice === "paper" && computerChoice === "rock") {
-            humanScore++;
-        }
-        else if (userChoice != computerChoice) {
-            computerScore++
-        }
-        console.log("Human points: ", humanScore, "Computer points: ", computerScore)
-    }
+function setupGame() {
+
+    let buttons = document.querySelectorAll(".buttons button");
+    buttons.forEach((button) => {
+        button.addEventListener('click', (event) => {
+
+            let userChoice = event.target.className;
+            let computerChoice = getComputerChoice();
+
+            userChoiceUI.textContent = userChoice;
+            computerChoiceUI.textContent = computerChoice;
+            playRound(userChoice, computerChoice);
+
+            counter++
+
+            if (counter >= 5) {
+
+                buttons.forEach(b => b.disabled = true);
+                playAgainButton.classList = "active";
+
+                if (humanScore > computerScore) {
+                    pedestal.textContent = "HEY YOU WON!!! CONGRATS";
+                    return;
+                }
+                else if (humanScore < computerScore) {
+
+                    pedestal.textContent = "Unfortunately Mr. Computer won.";
+                    return;
+                }
+                else {
+
+                    pedestal.textContent = "What a game, a TIE at last.";
+                    return;
+                }
 
 
-    function playGame(humanScore, computerScore) {
-        userChoice = getHumanChoice();
-        computerChoice = getComputerChoice();
 
-        console.log("You chose")
-        console.log(userChoice[0].toUpperCase() + userChoice.slice(1).toLowerCase())
+            }
 
-        console.log("Computer chose:", computerChoice[0].toUpperCase() + computerChoice.slice(1).toLowerCase())
 
-        playRound(userChoice, computerChoice);
-    }
 
-    let humanScore = 0;
-    let computerScore = 0;
+        })
+    })
 
-    let userChoice;
-    let computerChoice;
+}
 
-    let counter = 0;
+playAgainButton.addEventListener('click', () => {
+    humanScore = 0;
+    computerScore = 0;
+    counter = 0;
 
-    while (counter < 5) {
-        playGame(humanScore, computerScore)
-        counter++
-    }
+    humanScoreUI.textContent = humanScore;
+    computerScoreUI.textContent = computerScore;
+    userChoiceUI.textContent = "nothing";
+    computerChoiceUI.textContent = "nothing";
 
-    if (humanScore > computerScore) {
-        console.log("CONGRATULATIONS YOU WON!!!");
-    }
-    else if (humanScore < computerScore) {
-        console.log("Unfortunate Mr. Computer won.")
-    }
-    else {
-        console.log("A TIE")
-    }
+    pedestal.textContent = "Let's play again!";
+
+    playAgainButton.classList.remove("active")
+
+    document.querySelectorAll(".buttons button").forEach(b => b.disabled = false);
+})
+
+setupGame()
+
+
