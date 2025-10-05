5a746fc0ec8142dc5dc68dd49dda93a4aaa97dfc
LukaSirotkovic <lukasirotkovic5@gmail.com>
2025-10-05 20:21:23 +0200

    Made few tasks regarding javascript array methods

---
+++ javascript-test/getAvarageAge.js
@@ -0,0 +1,13 @@
+function getAverageAge(users) {
+    return users.reduce((previous, current) =>
+        previous + current.age, 0) / users.length
+}
+
+
+let john = { name: "John", age: 25 };
+let pete = { name: "Pete", age: 30 };
+let mary = { name: "Mary", age: 29 };
+
+let arr = [john, pete, mary];
+
+console.log(getAverageAge(arr)); // (25 + 30 + 29) / 3 = 28
---
+++ javascript-test/mapToObjects.js
@@ -0,0 +1,28 @@
+let john = { name: "John", surname: "Smith", id: 1 };
+let pete = { name: "Pete", surname: "Hunt", id: 2 };
+let mary = { name: "Mary", surname: "Key", id: 3 };
+
+let users = [john, pete, mary];
+
+let usersMapped = users.map(item => {
+
+    id = item.id,
+    fullName = item.name + ' ' + item.surname
+
+    console.log(id)
+
+    return { fullNamen, id }
+})
+
+console.log(usersMapped)
+
+/*
+usersMapped = [
+  { fullName: "John Smith", id: 1 },
+  { fullName: "Pete Hunt", id: 2 },
+  { fullName: "Mary Key", id: 3 }
+]
+*/
+
+console.log(usersMapped[0].id) // 1
+console.log(usersMapped[0].fullName) // John Smith
